3185104e57830c995fc69e1e167edc3272f98226
olaalnajjar <ola.alnajjar@hotmail.com>
2022-06-14 17:10:43 +0200

    Database
    Added more names to database

--- pages/database.js
+++ pages/database.js
@@ -26,8 +26,125 @@ var ourData = [{
     "loggedIn": true,
     "Admin" :false
 },{
+    "username": "Farida",
+    "displayName": "Farida Mahamed",
+    "profilePic": "../../assets/profiles/profile-4.png",
+    "email": "123@456.com",
+    "password": "12345",
+    "phoneNum": "0123456789",
+    "loggedIn": false,
+    "Admin" :false
+},{
+    "username": "Nour",
+    "displayName": "Nour Mohamed",
+    "profilePic": "../../assets/profiles/profile-4.png",
+    "email": "123@456.com",
+    "password": "12345",
+    "phoneNum": "0123456789",
+    "loggedIn": false,
+    "Admin" :false
+},{
+    "username": "farah",
+    "displayName": "Farah Kamaly",
+    "profilePic": "../../assets/profiles/profile-4.png",
+    "email": "123@456.com",
+    "password": "12345",
+    "phoneNum": "0123456789",
+    "loggedIn": false,
+    "Admin" :false
+},{
+    "username": "mariam",
+    "displayName": "Mariam Tamer",
+    "profilePic": "../../assets/profiles/profile-4.png",
+    "email": "123@456.com",
+    "password": "12345",
+    "phoneNum": "0123456789",
+    "loggedIn": false,
+    "Admin" :false
+},{
     "username": "hossam",
     "displayName": "Hossam Mansour",
+    "profilePic": "../../assets/profiles/profile-4.png",
+    "email": "123@456.com",
+    "password": "12345",
+    "phoneNum": "0123456789",
+    "loggedIn": false,
+    "Admin" :false
+},{
+    "username": "salma",
+    "displayName": "Salma Osama",
+    "profilePic": "../../assets/profiles/profile-4.png",
+    "email": "123@456.com",
+    "password": "12345",
+    "phoneNum": "0123456789",
+    "loggedIn": false,
+    "Admin" :false
+},{
+    "username": "hana",
+    "displayName": "Hana Ahmed",
+    "profilePic": "../../assets/profiles/profile-4.png",
+    "email": "123@456.com",
+    "password": "12345",
+    "phoneNum": "0123456789",
+    "loggedIn": false,
+    "Admin" :false
+},{
+    "username": "jana",
+    "displayName": "Jana Khaled",
+    "profilePic": "../../assets/profiles/profile-4.png",
+    "email": "123@456.com",
+    "password": "12345",
+    "phoneNum": "0123456789",
+    "loggedIn": false,
+    "Admin" :false
+},{
+    "username": "mahmoud",
+    "displayName": "Mahmoud Mohamed",
+    "profilePic": "../../assets/profiles/profile-4.png",
+    "email": "123@456.com",
+    "password": "12345",
+    "phoneNum": "0123456789",
+    "loggedIn": false,
+    "Admin" :false
+},{
+    "username": "mohab",
+    "displayName": "Mohab Galal",
+    "profilePic": "../../assets/profiles/profile-4.png",
+    "email": "123@456.com",
+    "password": "12345",
+    "phoneNum": "0123456789",
+    "loggedIn": false,
+    "Admin" :false
+},{
+    "username": "omar",
+    "displayName": "Omar Ahmed",
+    "profilePic": "../../assets/profiles/profile-4.png",
+    "email": "123@456.com",
+    "password": "12345",
+    "phoneNum": "0123456789",
+    "loggedIn": false,
+    "Admin" :false
+},{
+    "username": "shehab",
+    "displayName": "Shehab Adel",
+    "profilePic": "../../assets/profiles/profile-4.png",
+    "email": "123@456.com",
+    "password": "12345",
+    "phoneNum": "0123456789",
+    "loggedIn": false,
+    "Admin" :false
+},{
+    "username": "amr",
+    "displayName": "Amr Ahmed",
+    "profilePic": "../../assets/profiles/profile-4.png",
+    "email": "123@456.com",
+    "password": "12345",
+    "phoneNum": "0123456789",
+    "loggedIn": false,
+    "Admin" :false
+},{
+    "username": "ahmed",
+    "displayName": "Ahmed Wael",
     "profilePic": "../../assets/profiles/profile-4.png",
     "email": "123@456.com",
     "password": "12345",
