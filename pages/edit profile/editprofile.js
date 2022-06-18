54fa19c0aae8766719672219555f41c7844d9bc6
Ingy-Mysara <67437146+Ingy-Mysara@users.noreply.github.com>
2022-06-18 13:18:14 +0200

    added new feature in edit profile

--- pages/edit profile/editprofile.js
+++ pages/edit profile/editprofile.js
@@ -1,40 +1,44 @@
 for (i = 0; i < ourData.length; i++) {
-    if(ourData[i].loggedIn==true){
+    if (ourData[i].loggedIn == true) {
         profileUser = ourData[i];
     }
 }
 
-console.log(profileUser.profilePic);
+console.log(profileUser);
+console.log(ourData);
 if (profileUser) {
     console.log(document.querySelector("#profile-avatar").src);
     document.getElementById("profile-avatar").src = profileUser.profilePic;
     document.getElementById("profile-name").textContent = profileUser.displayName;
     document.getElementById("profile-username").textContent = profileUser.username;
 
-    document.getElementById("profilehyperlink").href = "../profile/Profile.html?"+profileUser.username;
-    document.getElementById("profileURL").href = "../profile/Profile.html?"+profileUser.username;
-    document.getElementById("postLink").href = "../makepost/makepost.html?"+profileUser.username;
-    document.getElementById("searchhyperlink").href = "../search and explore/search.html?"+profileUser.username;
+    document.getElementById("profilehyperlink").href = "../profile/Profile.html?" + profileUser.username;
+    document.getElementById("profileURL").href = "../profile/Profile.html?" + profileUser.username;
+    document.getElementById("postLink").href = "../makepost/makepost.html?" + profileUser.username;
+    document.getElementById("searchhyperlink").href = "../search and explore/search.html?" + profileUser.username;
 
-    
-    
-    console.log("finished");
+
+    document.getElementById("username").value = profileUser.username;
+    document.getElementById("displayname").value = profileUser.displayName;
+    document.getElementById("imgsrc").src = profileUser.profilePic;
+    document.getElementById("email").value = profileUser.email;
+    document.getElementById("password").value = profileUser.password;
+    document.getElementById("phonenumber").value = profileUser.phoneNum;
 
 }
 
 
-for (i = 0; i < ourData.length; i++) {
+/*for (i = 0; i < ourData.length; i++) {
     if (ourData[i].loggedIn) {
         document.getElementById("imgsrc").src = ourData[i].profilePic;
     }
-}
+}*/
 
 
 function editData() {
     console.log("submit button clicked");
     for (var i = 0; i < ourData.length; i++) {
         if (ourData[i].loggedIn == true) {
-            //ourData[datacount] = new Object();
             if (document.getElementById("username").value != "") {
                 ourData[i].username = document.getElementById("username").value;
             }
@@ -53,7 +57,7 @@ function editData() {
             if (document.getElementById("phonenumber").value != "") {
                 ourData[i].phoneNum = document.getElementById("phonenumber").value;
             }
-            //datacount++;
+            console.log(profileUser);
             console.log(ourData);
             alert("Your data is updated successfully!");
         }
